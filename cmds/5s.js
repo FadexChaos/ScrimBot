de4efc60be5d75c10cc6f8581691bb0ea03f0adf
Skylar Hefner <skylarhefner531@gmail.com>
2019-01-26 00:58:19 -0500

    New Project

---
+++ cmds/5s.js
@@ -0,0 +1,12 @@
+
+
+module.exports.run = async (bot,message,args) => {
+
+    message.channel.send("Next scrim starting in 5 seconds.");
+
+
+}
+
+module.exports.help = {
+    name: "5s"
+}
---
+++ cmds/60s.js
@@ -0,0 +1,13 @@
+
+
+module.exports.run = async (bot,message,args) => {
+  
+    message.channel.send("Next scrim starting in 60 seconds.")
+
+
+}
+
+
+module.exports.help = {
+    name: "60s"
+}
